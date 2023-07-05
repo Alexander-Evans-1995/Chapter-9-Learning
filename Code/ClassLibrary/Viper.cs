8cb5a9fd5b379381c0442ffa56b55673badccc22
Kirisamebiggestfan <136210082+Kirisamebiggestfan@users.noreply.github.com>
2023-07-05 13:34:46 -0500

    Practiced Writing to Streams

---
+++ Code/ClassLibrary/Viper.cs
@@ -0,0 +1,6 @@
+namespace Chapter09;
+
+public static class Viper {
+    // define an array of Viper pilot call signs
+    public static string[] Callsigns = new[] {"Husker", "Starbuck", "Apollo", "Boomer", "Bulldog", "Athena", "Helo", "Racetrack"};
+}
--- Code/Program/Program.cs
+++ Code/Program/Program.cs
@@ -1,7 +1,26 @@
 ﻿namespace Chapter09;
+using static System.Console;
+using System.Xml;
+using static System.Environment;
+using static System.IO.Path;
+using static System.IO.Directory;
 
-class Program {
+public partial class Program {
     public static void Main(string[] args) {
-        PrintTest.Ping();
+        //PrintTest.Ping();
+        //WritingToTextStreamPractice.Run();
+        //WritingToTextStreamPractice.Dispose();
+    }
+}
+
+public partial class Program {
+    public static void SectionTitle(string title)
+    {
+        ConsoleColor previousColor = ForegroundColor;
+        ForegroundColor = ConsoleColor.Yellow;
+        WriteLine("*");
+        WriteLine($"* {title}");
+        WriteLine("*");
+        ForegroundColor = previousColor;
     }
 }
